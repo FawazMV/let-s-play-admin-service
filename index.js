78de94327f5187a73f78518621d27f4a8673db68
Muhammed Fawaz MV <mvfawazmfz@gmail.com>
2023-04-10 15:02:13 +0530

    frontend path changed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config()
 const app = express()
 app.use(express.json());
 const corsOptions = {
-    origin: ['https://let-s-play.onrender.com', 'https://master.d3djpoiqsdvkks.amplifyapp.com'],
+    origin: ['https://let-s-play.onrender.com', 'https://master.des0fpb6pp73d.amplifyapp.com/'],
     methods: 'GET,POST,PATCH,PUT,DELETE',
     preflightContinue: true,
     optionsSuccessStatus: 200,
